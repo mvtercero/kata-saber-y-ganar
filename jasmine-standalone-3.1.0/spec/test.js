f38b772c74d24ffd8bdd93764dae73935dca0d93
mvtercero <mvtercero85@gmail.com>
2018-05-03 17:52:20 +0200

    Fail test resta puntos si fallo muy despacio undefined because false

--- jasmine-standalone-3.1.0/spec/test.js
+++ jasmine-standalone-3.1.0/spec/test.js
@@ -4,8 +4,11 @@
 *
 *      Si acierto pregunta en menos de 2 segundos - sumo 2 puntos
 *          (0 puntos, pregunta correcta, 1 segundo) -> 2 puntos
-*          (1 punto, correcta, 1 segundo) -> 3 puntos
+*          (2 puntos, correcta, 1 segundo) -> 4 puntos
 *      Si fallo pregunta en mas de 10 segundos - resto 2 puntos
+            (4 puntos, incorrecta, 12 segundos) -> 2 puntos
+            (2 puntos, incorrecta, 16 segundos) -> 0 puntos
+
 *      Si acierto pregunta entre 2 y 10 segundos - sumo 1 punto
             (1 punto, correcta, 5 segundos) -> 2 puntos
 *      Si acierto y tardo mas de 10 segundos - 0 puntos
@@ -28,4 +31,8 @@ describe('calculo de marcador', function(){
         expect(recalcularMarcador(0, true, 1)).toBe(2);
         expect(recalcularMarcador(2, true, 1)).toBe(4);
     });
+
+    it("resta puntos si fallo muy despacio", function() {
+        expect(recalcularMarcador(4, false, 12)).toBe(2);
+    })
 });
