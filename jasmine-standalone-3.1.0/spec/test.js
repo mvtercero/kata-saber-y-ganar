c3950a8aff2cdd78552d97868f4197b5a56942f0
mvtercero <mvtercero85@gmail.com>
2018-05-04 09:51:39 +0200

    Fail test no contestar a la pregunta en más de 20 segundos

--- jasmine-standalone-3.1.0/spec/test.js
+++ jasmine-standalone-3.1.0/spec/test.js
@@ -6,15 +6,18 @@
 *          (0 puntos, pregunta correcta, 1 segundo) -> 2 puntos
 *          (2 puntos, correcta, 1 segundo) -> 4 puntos
 *      Si fallo pregunta en mas de 10 segundos - resto 2 puntos
-            (4 puntos, incorrecta, 12 segundos) -> 2 puntos
+            (5 puntos, incorrecta, 12 segundos) -> 2 puntos
 
 *      Si acierto pregunta entre 2 y 10 segundos - sumo 1 punto
             (1 punto, correcta, 5 segundos) -> 2 puntos
 *      Si acierto y tardo mas de 10 segundos - 0 puntos
+            (2 puntos, correcta, 15 segundos) -> 2 puntos*
 *      Si fallo antes de 10 segundos - resto 1 punto
+            (3 puntos, incorrecta, 8 segundos) -> 2 puntos
 
 *      No se puede pasar sin responder
 *      Si en 20 segundos no has respondido , pasa a siguiente pregunta y pierdes 3 punto
+            ()
 *
 *
 * */
@@ -29,6 +32,8 @@ describe('calculo de marcador', function(){
             return puntos -2;
         } else if(!esCorrecta && tiempo <=10) {
             return puntos -1;
+        } else if (null && tiempo >= 20) {
+            return puntos -3;
         }
     }
 
@@ -40,6 +45,9 @@ describe('calculo de marcador', function(){
 
     it("resta puntos si fallo muy despacio", function() {
         expect(recalcularMarcador(5, false, 12)).toBe(3);
-        expect(recalcularMarcador(3, false, 8)).toBe(1);
+        expect(recalcularMarcador(3, false, 8)).toBe(2);
+    });
+    it("no contestar a la pregunta", function() {
+        expect(recalcularMarcador(2, null, 23)).toBe(-1);
     });
 });
