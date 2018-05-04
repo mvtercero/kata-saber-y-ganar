22769db274cd5be97417f8b2ccd835cc5ea6f539
mvtercero <mvtercero85@gmail.com>
2018-05-04 09:18:18 +0200

    Fail expect entre 2 y 10 segundos

--- jasmine-standalone-3.1.0/spec/test.js
+++ jasmine-standalone-3.1.0/spec/test.js
@@ -7,32 +7,34 @@
 *          (2 puntos, correcta, 1 segundo) -> 4 puntos
 *      Si fallo pregunta en mas de 10 segundos - resto 2 puntos
             (4 puntos, incorrecta, 12 segundos) -> 2 puntos
-            (2 puntos, incorrecta, 16 segundos) -> 0 puntos
 
 *      Si acierto pregunta entre 2 y 10 segundos - sumo 1 punto
             (1 punto, correcta, 5 segundos) -> 2 puntos
 *      Si acierto y tardo mas de 10 segundos - 0 puntos
 *      Si fallo antes de 10 segundos - resto 1 punto
+
 *      No se puede pasar sin responder
 *      Si en 20 segundos no has respondido , pasa a siguiente pregunta y pierdes 3 punto
 *
 *
 * */
 
-
 describe('calculo de marcador', function(){
     function recalcularMarcador(puntos, esCorrecta, tiempo){
         if (esCorrecta && tiempo <= 2){
             return puntos + 2;
+        } else if (esCorrecta && tiempo <= 10) {
+            return puntos + 1;
         }
     }
 
     it("suma mas puntos si acierta muy rapido", function(){
         expect(recalcularMarcador(0, true, 1)).toBe(2);
         expect(recalcularMarcador(2, true, 1)).toBe(4);
+        expect(recalcularMarcador(4, true, 5)).toBe(4);
     });
 
-    it("resta puntos si fallo muy despacio", function() {
+    xit("resta puntos si fallo muy despacio", function() {
         expect(recalcularMarcador(4, false, 12)).toBe(2);
-    })
+    });
 });
