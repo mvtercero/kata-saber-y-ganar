52b3c796acf1955a2c6ccbbed85c726c5718570a
mvtercero <mvtercero85@gmail.com>
2018-05-04 10:17:56 +0200

    Fixed null

--- jasmine-standalone-3.1.0/spec/test.js
+++ jasmine-standalone-3.1.0/spec/test.js
@@ -17,37 +17,40 @@
 
 *      No se puede pasar sin responder
 *      Si en 20 segundos no has respondido , pasa a siguiente pregunta y pierdes 3 punto
-            ()
 *
 *
 * */
 
 describe('calculo de marcador', function(){
-    function recalcularMarcador(puntos, esCorrecta, tiempo){
-        if (esCorrecta && tiempo <= 2){
+    function recalcularMarcador(puntos, existeRespuesta, esCorrecta, tiempo){
+        if (existeRespuesta && esCorrecta && tiempo <= 2){
             return puntos + 2;
-        } else if (esCorrecta && tiempo <= 10) {
+        } else if (existeRespuesta && esCorrecta && tiempo <= 10) {
             return puntos + 1;
-        } else if (!esCorrecta && tiempo >10) {
+        } else if (existeRespuesta && esCorrecta && tiempo <10) {
+            return puntos;
+        } else if (existeRespuesta && !esCorrecta && tiempo >10) {
             return puntos -2;
-        } else if(!esCorrecta && tiempo <=10) {
+        } else if(existeRespuesta && !esCorrecta && tiempo <=10) {
             return puntos -1;
-        } else if (null && tiempo >= 20) {
+        } else if (!existeRespuesta && tiempo >= 20) {
             return puntos -3;
         }
     }
 
     it("suma mas puntos si acierta muy rapido", function(){
-        expect(recalcularMarcador(0, true, 1)).toBe(2);
-        expect(recalcularMarcador(2, true, 1)).toBe(4);
-        expect(recalcularMarcador(4, true, 5)).toBe(5);
+        expect(recalcularMarcador(0, true, true, 1)).toBe(2);
+        expect(recalcularMarcador(2, true, true, 1)).toBe(4);
+        expect(recalcularMarcador(4, true, true, 5)).toBe(5);
+        //expect(recalcularMarcador(5, true, true, 15)).toBe(5);
     });
 
     it("resta puntos si fallo muy despacio", function() {
-        expect(recalcularMarcador(5, false, 12)).toBe(3);
-        expect(recalcularMarcador(3, false, 8)).toBe(2);
+        expect(recalcularMarcador(5, true, false, 12)).toBe(3);
+        expect(recalcularMarcador(3, true, false, 8)).toBe(2);
     });
     it("no contestar a la pregunta", function() {
-        expect(recalcularMarcador(2, null, 23)).toBe(-1);
+        expect(recalcularMarcador(2, false, true, 23)).toBe(-1);
+        expect(recalcularMarcador(2, false, false, 23)).toBe(-1);
     });
 });
